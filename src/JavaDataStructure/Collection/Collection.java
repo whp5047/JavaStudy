877a31dd708581eeee2188a02cd0e25833945133
whp5047 <whp5047@gmail.com>
2022-11-16 19:14:24 +0900

    #6 자바의 자료구조
    Collection 간단요약

---
+++ src/JavaDataStructure/Collection/Collection.java
@@ -0,0 +1,50 @@
+package JavaDataStructure.Collection;
+
+public class Collection{
+	
+	/* 컬랙션 프레임워크(Collection) :
+	 * 프로그램 구현에 필요한 자료구조(Data Structure)를 구현해 놓은 JDK 라이브러리
+	 * Java.util 패키지에 구현되어 있음
+	 * 개발에 소요되는 시간 절약과 최적화 된 알고리즘 사용가능
+	 * 여러 구현 클래스와 인터페이스의 활용에 대한 이해가 필요
+	 *
+	 *(자주사용하는 대표적인것)
+	 * Collection  
+	 * -> List -> ArrayList 
+	 * 		   -> Vector
+	 * 		   -> LinkedList
+	 * -> Set (집합 -> 중복허용x) -> HashSet
+	 * 		  				   -> TreeSet 
+	 * 
+	 * Map 
+	 * -> HashTable -> Properties
+	 * -> HashMap
+	 * -> TreeMap
+	 * 
+	 * 배열이나 그와 유사한 자료구조의 내부요소를 순회하는 객체 -> iterator  
+	 * set과같은 방식은 iterator 인터페이스의 함수를 사용해야함
+	 * 
+	 * Collection 인터페이스
+	 * 하나의 객체를 관리하기 위한 메서드가 선언된 인터페이스
+	 * 하위에 List와 Set 인터페이스가 있음
+	 * 
+	 * List 인터페이스
+	 * 객체를 순서에 따라 저장하고 관리하는데 필요한 메서드가 선언된 인터페이스
+	 * 자료구조 리스트(배열, 연결리스트)의 구현을 위한 인터페이스
+	 * 중복을 허용
+	 * 
+	 * Set 인터페이스
+	 * 순서와 상관 없이 중복을 허용하지 않고 유일한 값을 관리하는데 필요한 메서드가 선언
+	 * 아이디, 주민번호, 사번등을 관리하는데 유용
+	 * 저장된 순서와 출력되는 순서는 다를 수 있음
+	 * HashSet, TreeSet 등등
+	 * 
+	 * Map 인터페이스
+	 * 쌍(pair)로 이루어진 객체를 관리하는데 사용하는 메서드들이 선언된 인터페이스
+	 * 객체는 key-value의 쌍으로 이루어짐
+	 * key는 중복을 허용x
+	 * HashTable, HashMap, Properties, TreeMap 등이 Map인터페이스를 구현
+	 * 
+	 * 
+	 * */
+}
