00b4a1560ea35169fe9f5d8745fd5f18476c7f92
whp5047 <whp5047@gmail.com>
2022-12-05 22:45:26 +0900

    #8 최단거리구하기

---
+++ src/Algorithm/Graph/ShortestPath.java
@@ -0,0 +1,20 @@
+package Algorithm.Graph;
+
+public class ShortestPath {
+
+	public static void main(String[] args) {
+		MyGraph graph = new MyGraph(6);
+		graph.addEdges(0, 1, 1);
+		graph.addEdges(0, 2, 4);
+		graph.addEdges(1, 2, 2);
+		graph.addEdges(2, 3, 1);
+		graph.addEdges(3, 4, 8);
+		graph.addEdges(3, 5, 3);
+		graph.addEdges(4, 5, 4);
+
+		graph.calcShotestPath(1);
+		graph.showDistance(1);
+
+	}
+
+}
